5d32871bd186b0eb8270a9d5c70dfb5d0b7fc869
Nicolas Bruegger <brueggern@gmail.com>
2022-05-17 21:23:30 +0200

    first commit

---
+++ components/Tutorial.vue
@@ -0,0 +1,18 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
+    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+      <div class="bg-white overflow-hidden shadow sm:rounded-lg p-6">
+        <h1 class="text-3xl leading-7 font-semibold">
+          Nicolas Brügger
+        </h1>
+        <h2 class="mt-3 text-gray-600">
+          Developer, architect, project manager
+        </h2>
+        <blockquote class="mt-4 pt-4 text-gray-800 border-t border-dashed">&laquo;The perfect kind of architecture decision is the one which never has to be made.&raquo;</blockquote>
+        <div class='text-sm'><span class='font-bold'>Robert C. Martin</span> in Clean Architecture</div>
+      </div>
+    </div>
+  </div>
+</template>
