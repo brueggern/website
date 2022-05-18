9a556c5a4296205ca7bb72fa3beb81d279bd307c
brueggern <brueggern@gmail.com>
2022-05-18 08:07:10 +0200

    Bugfix welcome component

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,12 @@
 <template>
-  <Tutorial/>
+  <welcome />
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import Welcome from "~/components/Welcome.vue";
 
-export default Vue.extend({})
+export default Vue.extend({
+  components: {Welcome}
+})
 </script>
