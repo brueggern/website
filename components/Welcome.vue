8db74add6f6c2efdca15501a7dc77fb0e1e01a8b
brueggern <brueggern@gmail.com>
2022-07-08 09:21:09 +0200

    Avatar

--- components/Welcome.vue
+++ components/Welcome.vue
@@ -1,17 +1,28 @@
 <!-- Please remove this file from your project -->
 <template>
-  <div class="relative flex items-center justify-center min-h-screen bg-gradient-to-r from-background-purple to-background-magenta">
+  <div
+    class="relative flex items-center justify-center min-h-screen bg-gradient-to-r from-background-purple to-background-magenta">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
     <div class="max-w-4xl mx-auto text-center">
-      <h1 class="text-5xl font-roboto font-bold text-white mb-4">
-        Nicolas Brügger
-      </h1>
-      <div class="bg-gradient-to-r from-card-pink to-card-blue text-white overflow-hidden shadow rounded-lg px-4 py-3 mx-4 sm:px-6 sm:py-5">
+          <img
+            src="~/assets/Avatar.jpg"
+            class="w-64 rounded-full mx-auto p-4"
+            alt="image"
+          />
+          <h1 class="text-5xl font-roboto font-bold text-white mx-auto p-4">
+            Nicolas Brügger
+          </h1>
+      <div
+        class="bg-gradient-to-r from-card-pink to-card-blue text-white overflow-hidden shadow rounded-lg px-4 py-3 mx-4 sm:px-6 sm:py-5">
         <h2 class="text-xl font-robotoMono font-extralight">
           Developer, architect, project manager
         </h2>
-        <blockquote class="mt-4 pt-4 border-t border-dashed border-pink-300 font-roboto font-extralight">&laquo;The perfect kind of architecture decision is the one which never has to be made.&raquo;</blockquote>
-        <div class="text-sm font-roboto font-extralight"><span class='font-bold'>Robert C. Martin</span> in Clean Architecture</div>
+        <blockquote class="mt-4 pt-4 border-t border-dashed border-pink-300 font-roboto font-extralight">&laquo;The
+          perfect kind of architecture decision is the one which never has to be made.&raquo;
+        </blockquote>
+        <div class="text-sm font-roboto font-extralight"><span class='font-bold'>Robert C. Martin</span> in Clean
+          Architecture
+        </div>
       </div>
     </div>
   </div>
