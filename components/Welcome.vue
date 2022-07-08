80b6f1684dcaae0c1bcc5281612c2a3375d7b6f1
brueggern <brueggern@gmail.com>
2022-07-08 08:36:10 +0200

    Layout

--- components/Welcome.vue
+++ components/Welcome.vue
@@ -1,17 +1,17 @@
 <!-- Please remove this file from your project -->
 <template>
-  <div class="relative flex items-top justify-center min-h-screen bg-gradient-to-r from-background-purple to-background-magenta sm:items-center sm:pt-0">
+  <div class="relative flex items-center justify-center min-h-screen bg-gradient-to-r from-background-purple to-background-magenta">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
-    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
-      <div class="bg-gradient-to-r from-card-pink to-card-blue text-white overflow-hidden shadow sm:rounded-lg p-6">
-        <h1 class="text-3xl leading-7 font-semibold">
-          Nicolas Brügger
-        </h1>
-        <h2 class="mt-3">
+    <div class="max-w-4xl mx-auto text-center">
+      <h1 class="text-5xl font-roboto font-bold text-white mb-4">
+        Nicolas Brügger
+      </h1>
+      <div class="bg-gradient-to-r from-card-pink to-card-blue text-white overflow-hidden shadow rounded-lg px-4 py-3 mx-4 sm:px-6 sm:py-5">
+        <h2 class="text-xl font-robotoMono font-extralight">
           Developer, architect, project manager
         </h2>
-        <blockquote class="mt-4 pt-4 border-t border-dashed text-purple-100">&laquo;The perfect kind of architecture decision is the one which never has to be made.&raquo;</blockquote>
-        <div class="text-sm text-purple-200"><span class='font-bold'>Robert C. Martin</span> in Clean Architecture</div>
+        <blockquote class="mt-4 pt-4 border-t border-dashed border-pink-300 font-roboto font-extralight">&laquo;The perfect kind of architecture decision is the one which never has to be made.&raquo;</blockquote>
+        <div class="text-sm font-roboto font-extralight"><span class='font-bold'>Robert C. Martin</span> in Clean Architecture</div>
       </div>
     </div>
   </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 module.exports = {
   theme: {
+    fontFamily: {
+      roboto: ["Roboto"],
+      robotoMono: ["Roboto Mono"],
+    },
     colors: {
       "background-purple": '#25043a',
-      "background-magenta": '#34042c',
+      "background-magenta": '#3e1d4f',
       "card-pink": "#ab0954",
-      "card-blue": "#670737",
+      "card-blue": "#7e0a45",
     }
   },
   plugins: [],
